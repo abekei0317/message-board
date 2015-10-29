b7c77906a946421ec4482af9b7afbc118421f4fe
abekei0317 <abekei0317@gmail.com>
2015-10-29 06:52:30 +0000

    commit

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,19 +1,21 @@
 class MessagesController < ApplicationController
+  before_action :set_message, only: [edit, :update]
   def index
     # Messageを全て取得する。
     @messages = Message.all
     @message = Message.new
   end
+ 
+  def edit
+  end
   
-  def create
-    @message = Message.new(message_params)
-    if  @message.save
-      redirect_to root_path , notice: 'メッセージを保存しました'
+  def update
+    if @message/update(message_params)
+      # 保存に成功した場合はトップページへリダイレクト
+      redirect_to root_path , notice: 'メッセージを編集しました'
     else
-      # メッセージが保存できなかった時
-      @messages = Message.all
-      flash.now[:alert] = "メッセージの保存に失敗しました。"
-      render 'index'
+      # 保存に失敗した場合は編集画面へ戻す
+      render 'edit'
     end
   end
   
@@ -21,7 +23,11 @@ class MessagesController < ApplicationController
   def message_params
     params.require(:message).permit(:name, :body)
   end
+
+
+  def set_message
+    @message = Message.find(params[:id])
+  end
 end
 
-  
   
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   root 'messages#index'
-  resources :messages , only: [:create]
+  resources :messages , except: [:index, :new]
 
   # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
+  # See how all your rourates lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
